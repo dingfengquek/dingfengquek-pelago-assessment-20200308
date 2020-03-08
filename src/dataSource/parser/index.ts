69725fa60c74e2a79fe9a7d5e41d907e8389fb03
Dingfeng Quek <dingfeng@sg.ibm.com>
2020-03-08 16:42:57 +0800

    add dataSource/parser parseListData

--- src/dataSource/parser/index.ts
+++ src/dataSource/parser/index.ts
@@ -5,6 +5,49 @@ import { parseToLines } from './stringToLines';
 import { parseToPropGroups } from './lineToPropGroup';
 import { propGroupToPackageX } from './mapParserToApp';
 
+export interface ListDataElement {
+    name: string;
+    version: string;
+}
+
+export const parseListData = (input: string): rr.Result<readonly ListDataElement[], string> => {
+    // Parse to lines
+    const parseToLinesResult = parseToLines(input.split('\n'));
+    if (!parseToLinesResult.isOk) {
+        return rr.err(`[parseListData] Error while parsing input to lines:${parseToLinesResult.err}`);
+    }
+    const lines = parseToLinesResult.ok;
+    // ---
+    // Parse to prop groups, get the first one (there should only be 1)
+    const parseToPropGroupsResult = parseToPropGroups(lines);
+    if (!parseToPropGroupsResult.isOk) {
+        return rr.err(`[parseListData] Error while parsing Lines to PropGroups: ${parseToPropGroupsResult.err}`);
+    }
+    const propGroups = parseToPropGroupsResult.ok;
+    // ---
+    const listData: ListDataElement[] = [];
+    for (let i = 0; i < propGroups.length; i++) {
+        const propGroup = propGroups[i];
+        let name = '';
+        let version = '';
+        const unwalked = new Set(['Package', 'Version']);
+        for (const prop of propGroup) {
+            switch (prop.name) {
+                case 'Package': name = prop.value; unwalked.delete('Package'); break;
+                case 'Version': version = prop.value; unwalked.delete('Version'); break;
+            }
+            if (unwalked.size === 0) {
+                break;
+            }
+        }
+        if (unwalked.size > 0) {
+            return rr.err(`[parseListData] Error: Could not find the below properties in propGroup number ${i+1}, with props [${JSON.stringify(propGroup)}]`);
+        }
+        listData.push({ name, version });
+    }
+    // ---
+    return rr.ok(listData);
+};
 
 export const parsePackageDescriptionFile = (input: string): rr.Result<mmApp.PackageX, string> => {
     // Parse to lines
